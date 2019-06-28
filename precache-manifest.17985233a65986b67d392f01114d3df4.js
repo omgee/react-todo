faf287727663fa08f9663ada4d199ffd26b7d192
omgee <omgee@programmer.net>
2019-06-28 10:59:22 +0300

    Deploy #9

---
+++ precache-manifest.17985233a65986b67d392f01114d3df4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fd9329be5c41b117e04919cb19eeeb",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2e8db378ee44cec5aa6b",
+    "url": "/react-todo/static/js/2.1419d2ee.chunk.js"
+  },
+  {
+    "revision": "7f391003f70e59c39d10",
+    "url": "/react-todo/static/js/main.a106599f.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
